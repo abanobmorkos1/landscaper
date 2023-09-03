6ceaa7369976603fd9cf6b09cf8e8b634f653811
abanobmorkos1 <abanobmorkos11@gmail.com>
2023-09-03 16:54:26 -0400

    landscaper: user can use teeth to cut grass

--- landscaper.py
+++ landscaper.py
@@ -12,34 +12,6 @@ while(True):
                            
                             [3] Take the day off
                             """))
-    
     if (user_input == 1):
-        if game_data["HasScissors"]:
-            game_data["Money"] += 5  
-            print(f"Cutting grass with the scissors")
-        else:
-            game_data['Money'] += 1 
-        print(f"You now have ${game_data['Money']}")
-
-        
-    if (user_input == 2):
-        if game_data['Money'] >= 5:
-            game_data['Money'] -= 5
-            if game_data['HasScissors'] == True:
-                print("You bought rusty scissors and can now cut grass faster!")
-        else:
-            print("You don't have enough money to buy scissors!")
-    if(game_data["HasScissors"] == True):
-        int(input(""" 
-                    [1] buy a lawn mower
-
-                    """))
-    else:
-        print("You")
-        
-
-    if(user_input == 3):
-        game_data['quit'] == True
-        print("You quit the game successfully")
-        break
-        
+        game_data["Money"] += 1
+        print(f'You now have ${game_data["Money"]}')
